990d49ad0741208071a06270f7704fc2567e2bf3
Asmir Mehic <fdcmehic@gmail.com>
2026-03-27 09:05:47 +0100

    update db connection for Railway

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,8 @@ require('dotenv').config({ quiet: true });
 let { Pool } = require('pg');
 
 let pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'portfolio_cms',
-    password: process.env.DB_PASSWORD,
-    port: '5432'
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
